a29b94710f716b81e7bcb3f7ff6f8f01b4a789ef
hawk0722 <siroinekotokuroineko@gmail.com>
2023-09-26 18:39:07 +0900

    fix nsg rule

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "azurerm_network_security_group" "nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "21"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
